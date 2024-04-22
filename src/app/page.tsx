faffb16eac5d3dbb2ad2f5e58276815c66ecb438
Phalcin <phalcinhayes@gmail.com>
2024-04-22 11:58:09 +0000

    Committing changes before switching branches

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,3 @@
-// import Banner from "@/components/Banner";
-// import Products from "@/components/Products";
-
-// export default function Home() {
-//   return (
-//     <main>
-//       <Banner />
-//       <Products />
-//     </main>
-//   );
-// }
-
 import Banner from "@/components/Banner";
 import Products from "@/components/Products";
 
